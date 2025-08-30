3cc2e1565421cc6c50ff4df4e0573b9767454a19
Inderpreet Johar <johar.inderpreet@gmail.com>
2025-08-30 00:58:36 -0500

    Implemented DP-1

--- HouseRobber.java
+++ HouseRobber.java
@@ -16,15 +16,16 @@ public class HouseRobber {
     public int rob(int[] nums) {
         if (nums.length == 1) return nums[0]; //there's only one house on the street
 
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(dp[0], nums[1]);
+        int two = nums[0];
+        int one = Math.max(two, nums[1]);
 
         for (int i = 2; i < nums.length; i++) {
-            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+            int temp = one;
+            one = Math.max(nums[i] + two, one);
+            two = temp;
         }
 
-        return dp[nums.length - 1];
+        return one;
     }
 
     public static void main(String[] args) {
